0d8ca87b82d87c46f6555207d2220173eb825115
jonathanbenjamin13214 <jonathanbenjamin13214@gmail.com>
2026-02-22 07:04:43 -0500

    Add files via upload

---
+++ letter_or_not.py
@@ -0,0 +1,6 @@
+character = input("Enter a single character: ")
+
+if (character) = "abcdefghijklmnopqrstuvwxyz"
+    print(f"'{character}' is an English alphabet letter.")
+else:
+    print(f"'{character}' is NOT an English alphabet letter.")
